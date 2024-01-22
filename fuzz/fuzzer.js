664cb7d39c88784077830fbe7f5e4f52386885ed
Tim Strazzere <tim@strazzere.com>
2024-01-22 12:24:35 -0800

    Add test fuzzer

---
+++ fuzz/fuzzer.js
@@ -0,0 +1,22 @@
+import { Packet } from '../dist/packet.js';
+
+function fuzz(buffer) {
+    try {
+      const fuzzing = Packet.fromBuffer(buffer)
+      fuzzing.isValid()
+      fuzzing.toHexString()
+      fuzzing.toLongString()
+      fuzzing.toShortString()
+    } catch (e) {
+        if (e.message.indexOf('Buffer length smaller than minimum size allowed for valid packet') !== -1 ||
+            e.message.indexOf('Unexpected magic identifier') !== -1 ||
+            e.message.indexOf('Packet length larger than provided buffer') !== -1) {
+        } else {
+            throw e;
+        }
+    }
+}
+
+export default {
+    fuzz
+};
