9adec506a5ad69513919262f5ddb865c4adf610b
Tim Strazzere <tim@strazzere.com>
2024-01-23 11:39:28 -0800

    Enable cjs and esm, publish, 2.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -14,6 +14,17 @@ const bundle = [
     plugins: [typescript()],
   },
   {
+    input: './dist/packet.js',
+    output: [
+      {
+        file: './dist/bundle.cjs',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript()],
+  },
+  {
     input: './dist/types.d.ts',
     output: {
       file: './dist/index.d.ts',
